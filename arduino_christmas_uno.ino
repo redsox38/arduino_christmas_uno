27ae12bde90e1fd6a1212460e69925636a3c76d5
Todd Merritt <tmerritt@email.arizona.edu>
2017-12-25 19:57:10 -0700

    Major revision. Move reads outside of lock so  we ignore reads that come in while we're in mid sequence. Update delay interval. Add missing flah_ct definition.

--- arduino_christmas_uno.ino
+++ arduino_christmas_uno.ino
@@ -22,7 +22,9 @@
 SoftwareSerial BTSerial(HC_05_TXD_ARDUINO_RXD, HC_05_RXD_ARDUINO_TXD); // RX | TX
  
 int relayLock;
-int flash_ct;
+int flash_ct = 3;
+
+int delay_time = 300;
 
 void setup() 
 { 
@@ -62,35 +64,40 @@ void loop()
     
     // listen for communication from the BT module and then write it to the serial monitor
     if ( BTSerial.available() > 0)   {  
+        c = BTSerial.read(); 
         if (relayLock == 0) {
             relayLock = 1;        
             digitalWrite(13, HIGH);
-            c = BTSerial.read(); 
             if  (c == '1') {
-              for (i = MIN_RELAY_PIN; i <= MAX_RELAY_PIN; i++) {
+              for (i = MIN_RELAY_PIN + 1; i <= MAX_RELAY_PIN; i++) {
+                digitalWrite(i - 1, HIGH);
+                delay(delay_time);
                 digitalWrite(i, HIGH);
-                delay(500);
-                digitalWrite(i + 1, HIGH);
-                delay(200);
-                digitalWrite(i, LOW);
+                delay((delay_time / 2));
+                digitalWrite(i - 1, LOW);
               }              
+              digitalWrite(MAX_RELAY_PIN, LOW);
             } else if (c == '2') {
+              PORTD = B11111100;
+              delay(delay_time);
+              PORTD = B00000000;
+              delay(delay_time);
               for (i = MIN_RELAY_PIN; i <= MAX_RELAY_PIN; i++) {
                 digitalWrite(i, HIGH);
-                delay(400);
+                delay(delay_time);
                 digitalWrite(i, LOW);
               }
               for (i = MAX_RELAY_PIN; i >= MIN_RELAY_PIN; i--) {
                 digitalWrite(i, HIGH);
-                delay(700);
+                delay(200);
                 digitalWrite(i, LOW);
-              }              
+              }            
             } else if (c == '3') {
                for (i = 0; i < flash_ct; i++) {
-                 PORTD = B00111111;
-                 delay(200)
+                 PORTD = B11111100;
+                 delay(delay_time / 3);
                  PORTD = B00000000;
-                 delay(200);
+                 delay(delay_time / 3);
                }
             }           
             digitalWrite(13, LOW);
